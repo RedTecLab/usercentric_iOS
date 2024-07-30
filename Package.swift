ef5cd4223386b2141788b38cc168414b5a9cf7ba
AntonRomanchuk <iosradev@gmail.com>
2024-07-30 11:09:26 +0300

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "Usercentrics",
+    platforms: [
+        .iOS(.v11),
+        .tvOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "Usercentrics",
+            targets: ["Usercentrics"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "Usercentrics",
+            url: "https://github.com/RedTecLab/usercentric_iOS/Usercentrics-2.15.4.xcframework.zip",
+            checksum: "99a788ac05cd4464ef49518da0f89fb5a21466c0139393aaa522ba2bd21d2d6c"
+        ),
+    ]
+)
